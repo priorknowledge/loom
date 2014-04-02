95bcb86c0cba112c39f938c2fbca83950b18f211
Fritz Obermeyer <fobermeyer@salesforce.com>
2014-04-02 01:56:09 +0000

    Add gzip compression layer to message streams

--- src/protobuf.hpp
+++ src/protobuf.hpp
@@ -5,6 +5,7 @@
 #include <fcntl.h>
 #include <google/protobuf/io/coded_stream.h>
 #include <google/protobuf/io/zero_copy_stream_impl.h>
+#include <google/protobuf/io/gzip_stream.h>
 #include "common.hpp"
 
 namespace loom
@@ -49,6 +50,12 @@ inline bool parse_delimited (
 
 // see https://developers.google.com/protocol-buffers/docs/reference/cpp
 
+inline bool endswith (const char * filename, const char * ext)
+{
+    return strlen(filename) >= strlen(ext) and
+        strcmp(filename + strlen(filename) - strlen(ext), ext) == 0;
+}
+
 class InFileStream
 {
 public:
@@ -57,28 +64,36 @@ public:
     {
         fid_ = open(filename, O_RDONLY | O_NOATIME);
         LOOM_ASSERT(fid_ != -1, "failed to open values file");
-        raw_input_ = new google::protobuf::io::FileInputStream(fid_);
-        coded_input_ = new google::protobuf::io::CodedInputStream(raw_input_);
+        raw_ = new google::protobuf::io::FileInputStream(fid_);
+        if (endswith(filename, ".gz")) {
+            gzip_ = new google::protobuf::io::GzipInputStream(raw_);
+            coded_ = new google::protobuf::io::CodedInputStream(gzip_);
+        } else {
+            gzip_ = nullptr;
+            coded_ = new google::protobuf::io::CodedInputStream(raw_);
+        }
     }
 
     ~InFileStream ()
     {
-        delete coded_input_;
-        delete raw_input_;
+        delete coded_;
+        delete gzip_;
+        delete raw_;
         close(fid_);
     }
 
     template<class Message>
     bool try_read (Message & message)
     {
-        return parse_delimited(* coded_input_, message);
+        return parse_delimited(* coded_, message);
     }
 
 private:
 
     int fid_;
-    google::protobuf::io::ZeroCopyInputStream * raw_input_;
-    google::protobuf::io::CodedInputStream * coded_input_;
+    google::protobuf::io::FileInputStream * raw_;
+    google::protobuf::io::GzipInputStream * gzip_;
+    google::protobuf::io::CodedInputStream * coded_;
 };
 
 class OutFileStream
@@ -89,29 +104,35 @@ public:
     {
         fid_ = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
         LOOM_ASSERT(fid_ != -1, "failed to open values file");
-        raw_output_ = new google::protobuf::io::FileOutputStream(fid_);
-        coded_output_ =
-            new google::protobuf::io::CodedOutputStream(raw_output_);
+        raw_ = new google::protobuf::io::FileOutputStream(fid_);
+        if (endswith(filename, ".gz")) {
+            gzip_ = new google::protobuf::io::GzipOutputStream(raw_);
+            coded_ = new google::protobuf::io::CodedOutputStream(gzip_);
+        } else {
+            gzip_ = nullptr;
+            coded_ = new google::protobuf::io::CodedOutputStream(raw_);
+        }
     }
 
     ~OutFileStream ()
     {
-        delete coded_output_;
-        delete raw_output_;
+        delete coded_;
+        delete raw_;
         close(fid_);
     }
 
     template<class Message>
     void write (Message & message)
     {
-        return serialize_delimited(* coded_output_, message);
+        return serialize_delimited(* coded_, message);
     }
 
 private:
 
     int fid_;
-    google::protobuf::io::ZeroCopyOutputStream * raw_output_;
-    google::protobuf::io::CodedOutputStream * coded_output_;
+    google::protobuf::io::FileOutputStream * raw_;
+    google::protobuf::io::GzipOutputStream * gzip_;
+    google::protobuf::io::CodedOutputStream * coded_;
 };
 
 } // namespace protobuf
