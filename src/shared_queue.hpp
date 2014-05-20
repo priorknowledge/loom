1f49dc54a85a2fd004ce3f61dfda5b8e697f4512
Fritz Obermeyer <fobermeyer@salesforce.com>
2014-05-20 19:08:17 +0000

    Simplify SharedQueue (still broken)

--- src/shared_queue.hpp
+++ src/shared_queue.hpp
@@ -15,8 +15,7 @@
 #endif // LOOM_ASSUME_X86
 
 #if 0
-#define LOOM_DEBUG_QUEUE(message) \
-    LOOM_DEBUG(pending_counts() << ' ' << message);
+#define LOOM_DEBUG_QUEUE(message) LOOM_DEBUG(pendings() << ' ' << message);
 #else
 #define LOOM_DEBUG_QUEUE(message)
 #endif
@@ -30,9 +29,9 @@ class SharedQueue
     struct Envelope
     {
         Message message;
-        std::atomic<uint_fast64_t> pending_count;
+        std::atomic<uint_fast64_t> pending;
 
-        Envelope () : pending_count(0) {}
+        Envelope () : pending(0) {}
     };
 
     struct Guard
@@ -60,13 +59,19 @@ class SharedQueue
     Guard front_;
     Guard back_;
 
-    const Envelope & envelopes (size_t position) const
-    {
-        return envelopes_[position % size_plus_one_];  // TODO use mask
-    }
     Envelope & envelopes (size_t position)
     {
         return envelopes_[position % size_plus_one_];  // TODO use mask
+    }
+
+    std::vector<size_t> pendings () const
+    {
+        std::vector<size_t> counts;
+        counts.reserve(size_plus_one_);
+        for (size_t i = 0; i < size_plus_one_; ++i) {
+            counts.push_back(envelopes_[i].pending.load());
+        }
+        return counts;
     }
 
 public:
@@ -74,10 +79,9 @@ public:
     SharedQueue (size_t size) :
         envelopes_(new Envelope[size + 1]),
         size_plus_one_(size + 1),
-        position_(0),
-        front_(),
-        back_()
+        position_(0)
     {
+        assert_ready();
     }
 
     ~SharedQueue ()
@@ -92,7 +96,7 @@ public:
     {
         if (LOOM_DEBUG_LEVEL >= 2) {
             for (size_t i = 0; i < size_plus_one_; ++i) {
-                LOOM_ASSERT_EQ(envelopes(i).pending_count.load(), 0);
+                LOOM_ASSERT_EQ(envelopes_[i].pending.load(), 0);
             }
         }
     }
@@ -106,9 +110,8 @@ public:
     void producer_wait ()
     {
         LOOM_DEBUG_QUEUE("wait at " << (position_ % size_plus_one_));
-        size_t last_position = position_ + size_plus_one_ - 1;
-        Envelope & last = envelopes(last_position);
-        back_.wait(last.pending_count, [](size_t count) {
+        Envelope & last_to_finish = envelopes(position_ + size_plus_one_ - 1);
+        back_.wait(last_to_finish.pending, [](size_t count) {
             return count == 0;
         });
         assert_ready();
@@ -117,62 +120,39 @@ public:
     template<class Producer>
     void produce (const Producer & producer)
     {
-        LOOM_ASSERT2(size_plus_one_ > 1, "cannot use zero-length queue");
         LOOM_DEBUG_QUEUE("produce " << (position_ % size_plus_one_));
+        LOOM_ASSERT2(size_plus_one_ > 1, "cannot use zero-length queue");
 
         Envelope & fence = envelopes(position_ + 1);
-        back_.wait(fence.pending_count, [](size_t count) {
-            return count == 0;
-        });
+        back_.wait(fence.pending, [](size_t count) { return count == 0; });
 
         Envelope & envelope = envelopes(position_);
-        LOOM_ASSERT2(envelope.pending_count.load() == 0, "programmer error");
-        size_t consumer_count = producer(envelope.message);
         position_ += 1;
+        size_t consumer_count = producer(envelope.message);
+        LOOM_ASSERT2(consumer_count, "message sent to no consumers");
         store_barrier();
 
         std::unique_lock<std::mutex> lock(front_.mutex);
-        envelope.pending_count.store(consumer_count, std::memory_order_release);
+        envelope.pending.store(consumer_count, std::memory_order_release);
         front_.cond_variable.notify_all();
     }
 
     template<class Consumer>
     void consume (size_t position, const Consumer & consumer)
     {
-        if (LOOM_DEBUG_LEVEL >= 2) {
-            load_barrier();
-            LOOM_ASSERT_LE(position, position_);
-            LOOM_ASSERT_LE(position_, position + size_plus_one_ - 1);
-            LOOM_ASSERT(size_plus_one_ > 1, "cannot use zero-length queue");
-        }
         LOOM_DEBUG_QUEUE("consume " << (position % size_plus_one_));
+        LOOM_ASSERT2(size_plus_one_ > 1, "cannot use zero-length queue");
 
         Envelope & envelope = envelopes(position);
-        front_.wait(envelope.pending_count, [](size_t count){
-            return count != 0;
-        });
+        front_.wait(envelope.pending, [](size_t count){ return count != 0; });
 
         load_barrier();
         consumer(const_cast<const Message &>(envelope.message));
 
-        bool is_last_to_finish =
-            envelope.pending_count.fetch_sub(1, std::memory_order_acq_rel) == 1;
-        if (is_last_to_finish) {
+        if (envelope.pending.fetch_sub(1, std::memory_order_acq_rel) == 1) {
             std::unique_lock<std::mutex> lock(back_.mutex);
             back_.cond_variable.notify_one();
         }
-    }
-
-private:
-
-    std::vector<size_t> pending_counts () const
-    {
-        std::vector<size_t> counts;
-        counts.reserve(size_plus_one_);
-        for (size_t i = 0; i < size_plus_one_; ++i) {
-            counts.push_back(envelopes_[i].pending_count.load());
-        }
-        return counts;
     }
 };
 
