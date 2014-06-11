7734145c46aa7f5c41d2256c5d390e165dfc9cb1
Jonathan Glidden <jglidden@salesforce.com>
2014-06-11 21:43:14 +0000

    actually commit query server

---
+++ src/query.cc
@@ -0,0 +1,45 @@
+#include <loom/args.hpp>
+#include <loom/protobuf_stream.hpp>
+#include <loom/logger.hpp>
+#include <loom/loom.hpp>
+
+const char * help_message =
+"Usage: query CONFIG_IN MODEL_IN GROUPS_IN QUERIES_IN RESULTS_OUT LOG_OUT"
+"\nArguments:"
+"\n  CONFIG_IN     filename of config (e.g. config.pb.gz)"
+"\n  MODEL_IN      filename of model (e.g. model.pb.gz)"
+"\n  GROUPS_IN     dirname containing per-kind group files"
+"\n  QUERIES_IN    filename of queries stream (e.g. queries.pbs.gz)"
+"\n  RESULTS_OUT   filename of results stream (e.g. results.pbs.gz)"
+"\n  LOG_OUT       filename of log (e.g. log.pbs.gz)"
+"\n                or --none to not log"
+"\nNotes:"
+"\n  Any filename can end with .gz to indicate gzip compression."
+"\n  Any filename can be '-' or '-.gz' to indicate stdin/stdout."
+;
+
+int main (int argc, char ** argv)
+{
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+    Args args(argc, argv, help_message);
+    const char * config_in = args.pop();
+    const char * model_in = args.pop();
+    const char * groups_in = args.pop();
+    const char * queries_in = args.pop();
+    const char * results_out = args.pop();
+    const char * log_out = args.pop_optional_file();
+    args.done();
+
+    if (log_out) {
+        loom::logger.open(log_out);
+    }
+
+    const auto config = loom::protobuf_load<loom::protobuf::Config>(config_in);
+    loom::rng_t rng(config.seed());
+    loom::Loom engine(rng, config, model_in, groups_in);
+
+    engine.query(rng, queries_in, results_out);
+
+    return 0;
+}
---
+++ src/query_server.hpp
@@ -0,0 +1,131 @@
+#pragma once
+
+#include <loom/cross_cat.hpp>
+
+namespace loom
+{
+
+class QueryServer
+{
+public:
+
+    typedef protobuf::Query::Request Request;
+    typedef protobuf::Query::Response Response;
+    typedef CrossCat::Value Value;
+
+    QueryServer (const CrossCat & cross_cat) :
+        cross_cat_(cross_cat),
+        value_join_(cross_cat),
+        to_sample_(),
+        partial_values_(),
+        result_factors_(),
+        scores_(),
+        timer_()
+    {
+    }
+
+    void score_row (
+            rng_t & rng,
+            const Request & request,
+            Response & response);
+
+    void sample_row (
+            rng_t & rng,
+            const Request & request,
+            Response & response);
+
+private:
+
+    const CrossCat & cross_cat_;
+    CrossCat::ValueJoiner value_join_;
+    Value to_sample_;
+    std::vector<Value> partial_values_;
+    std::vector<std::vector<Value>> result_factors_;
+    VectorFloat scores_;
+    Timer timer_;
+};
+
+inline void QueryServer::score_row (
+        rng_t & rng,
+        const Request & request,
+        Response & response)
+{
+    Timer::Scope timer(timer_);
+
+    response.Clear();
+    response.set_id(request.id());
+    if (not cross_cat_.schema.is_valid(request.score().data())) {
+        response.set_error("invalid query data");
+        return;
+    }
+
+    cross_cat_.value_split(request.score().data(), partial_values_);
+
+    const size_t kind_count = cross_cat_.kinds.size();
+    for (size_t i = 0; i < kind_count; ++i) {
+        const Value & value = partial_values_[i];
+        auto & kind = cross_cat_.kinds[i];
+        const ProductModel & model = kind.model;
+        auto & mixture = kind.mixture;
+
+        mixture.score_value(model, value, scores_, rng);
+    }
+    (* response.mutable_score()).set_score(distributions::log_sum_exp(scores_));
+
+}
+
+inline void QueryServer::sample_row (
+        rng_t & rng,
+        const Request & request,
+        Response & response)
+{
+    Timer::Scope timer(timer_);
+
+    response.Clear();
+    response.set_id(request.id());
+    if (not cross_cat_.schema.is_valid(request.sample().data())) {
+        response.set_error("invalid request data");
+        return;
+    }
+    if (request.sample().data().observed_size() != request.sample().to_sample_size()) {
+        response.set_error("observed size != to_sample size");
+        return;
+    }
+    const size_t sample_count = request.sample().sample_count();
+    if (sample_count == 0) {
+        return;
+    }
+
+    cross_cat_.value_split(request.sample().data(), partial_values_);
+    * to_sample_.mutable_observed() = request.sample().to_sample();
+    result_factors_.resize(sample_count);
+    cross_cat_.value_split_observed(to_sample_, result_factors_.front());
+    std::fill(
+        result_factors_.begin() + 1,
+        result_factors_.end(),
+        result_factors_.front());
+
+    const size_t kind_count = cross_cat_.kinds.size();
+    for (size_t i = 0; i < kind_count; ++i) {
+        if (cross_cat_.schema.observed_count(result_factors_.front()[i])) {
+            const Value & value = partial_values_[i];
+            auto & kind = cross_cat_.kinds[i];
+            const ProductModel & model = kind.model;
+            auto & mixture = kind.mixture;
+
+            mixture.score_value(model, value, scores_, rng);
+            distributions::scores_to_probs(scores_);
+            const VectorFloat & probs = scores_;
+
+            for (auto & result_values : result_factors_) {
+                mixture.sample_value(model, probs, result_values[i], rng);
+            }
+        }
+    }
+
+    for (const auto & result_values : result_factors_) {
+        value_join_(* (* response.mutable_sample()).add_samples(), result_values);
+    }
+}
+
+} // namespace loom
