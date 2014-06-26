319c4f75a489840df192f2c4487772abab4b769c
Fritz Obermeyer <fobermeyer@salesforce.com>
2014-06-26 00:39:37 +0000

    Remove gratuitous test_parallel_ingest

--- loom/test/test_format.py
+++ loom/test/test_format.py
@@ -26,15 +26,12 @@
 # USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import os
-from itertools import izip
 from distributions.fileutil import tempdir
 from nose.tools import assert_equal
-import mock
-from distributions.tests.util import assert_close
 import loom.format
 import loom.util
 from loom.test.util import for_each_dataset, CLEANUP_ON_ERROR, assert_found
-from distributions.io.stream import json_load, protobuf_stream_load
+from distributions.io.stream import protobuf_stream_load
 
 
 @for_each_dataset
@@ -68,14 +65,17 @@ def test_make_encoding(schema, rows_csv, **unused):
 
 
 @for_each_dataset
-def test_import_rows(encoding, rows_csv, **unused):
+def test_import_rows(encoding, rows, rows_csv, **unused):
     with tempdir(cleanup_on_error=CLEANUP_ON_ERROR):
-        rows = os.path.abspath('rows.pbs.gz')
+        rows_pbs = os.path.abspath('rows.pbs.gz')
         loom.format.import_rows(
             encoding_in=encoding,
             rows_in=rows_csv,
-            rows_out=rows)
-        assert_found(rows)
+            rows_out=rows_pbs)
+        assert_found(rows_pbs)
+        expected_count = sum(1 for _ in protobuf_stream_load(rows))
+        actual_count = sum(1 for _ in protobuf_stream_load(rows_pbs))
+        assert_equal(actual_count, expected_count)
 
 
 @for_each_dataset
@@ -98,35 +98,3 @@ def test_export_rows(encoding, rows, **unused):
         expected_count = sum(1 for _ in protobuf_stream_load(rows))
         actual_count = sum(1 for _ in protobuf_stream_load(rows_pbs))
         assert_equal(actual_count, expected_count)
-
-
-@for_each_dataset
-def test_parallel_ingest(schema, rows_csv, **unused):
-    with tempdir(cleanup_on_error=CLEANUP_ON_ERROR):
-        expected_encoding = os.path.abspath('expected_encoding.json.gz')
-        expected_rows = os.path.abspath('expected_rows.pbs.gz')
-        actual_encoding = os.path.abspath('actual_encoding.json.gz')
-        actual_rows = os.path.abspath('actual_rows.pbs.gz')
-        with mock.patch('loom.util.THREADS', new=1):
-            loom.format.make_encoding(
-                schema_in=schema,
-                rows_in=rows_csv,
-                encoding_out=expected_encoding)
-            loom.format.import_rows(
-                encoding_in=expected_encoding,
-                rows_in=rows_csv,
-                rows_out=expected_rows)
-        with mock.patch('loom.util.THREADS', new=8):
-            loom.format.make_encoding(
-                schema_in=schema,
-                rows_in=rows_csv,
-                encoding_out=actual_encoding)
-            loom.format.import_rows(
-                encoding_in=actual_encoding,
-                rows_in=rows_csv,
-                rows_out=actual_rows)
-        assert_equal(json_load(actual_encoding), json_load(expected_encoding))
-        for actual, expected in izip(
-                protobuf_stream_load(actual_rows),
-                protobuf_stream_load(expected_rows)):
-            assert_close(actual, expected)
