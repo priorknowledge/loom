2de3169c0a70ff9701e24077a52e7fcc735e80c9
Jonathan Glidden <jglidden@salesforce.com>
2014-07-22 20:31:50 +0000

    document constants

--- loom/test/test_query_math.py
+++ loom/test/test_query_math.py
@@ -16,13 +16,22 @@ from nose.tools import (
 )
 # from nose import SkipTest
 import numpy as np
+import scipy.stats
 from loom.test.util import for_each_dataset, CLEANUP_ON_ERROR
 from itertools import product
 from test_query import get_protobuf_server
 from util import load_rows
 
 
-MIN_GOODNESS_OF_FIT = 1e-3
+GOF_EXP = 3
+MIN_GOODNESS_OF_FIT = 10. ** (-GOF_EXP)
+CONFIDENCE_INTERVAL = 1. - MIN_GOODNESS_OF_FIT
+Z_SCORE = scipy.stats.norm.ppf(CONFIDENCE_INTERVAL)
+
+SAMPLE_COUNT = 300
+
+# tests are inaccurate with highly imbalanced data
+MIN_CATEGORICAL_PROB = .01
 
 
 @for_each_dataset
@@ -41,7 +50,7 @@ def test_score_none(model, groups, encoding, **unused):
             assert_almost_equal(
                 query_server.score([None for _ in fnames]),
                 0.,
-                places=3)
+                places=GOF_EXP)
 
 
 @for_each_dataset
@@ -56,8 +65,8 @@ def test_mi_entropy_relations(model, groups, encoding, **unused):
         fnames = preql.feature_names
         feature_sets = [
             [fnames[0]],
+            [fnames[2]],
             [fnames[0], fnames[1]],
-            [fnames[2], fnames[3]],
         ]
         for fset1, fset2 in product(feature_sets, feature_sets):
             to_sample1 = preql.cols_to_sample(fset1)
@@ -66,28 +75,28 @@ def test_mi_entropy_relations(model, groups, encoding, **unused):
             mutual_info = query_server.mutual_information(
                 to_sample1,
                 to_sample2,
-                sample_count=100)
+                sample_count=SAMPLE_COUNT)
             entropy1 = query_server.entropy(
                 to_sample1,
-                sample_count=100)
+                sample_count=SAMPLE_COUNT)
             entropy2 = query_server.entropy(
                 to_sample2,
-                sample_count=100)
+                sample_count=SAMPLE_COUNT)
             entropy_joint = query_server.entropy(
                 to_sample,
-                sample_count=100)
+                sample_count=SAMPLE_COUNT)
             if to_sample1 == to_sample2:
                 measures = [mutual_info, entropy1, entropy2, entropy_joint]
                 for m1, m2 in product(measures, measures):
                     assert_less_equal(
                         abs(m1.mean - m2.mean),
-                        2.575 * np.sqrt(m1.variance + m2.variance))
+                        Z_SCORE * np.sqrt(m1.variance + m2.variance))
             expected = mutual_info.mean
             actual = entropy1.mean + entropy2.mean - entropy_joint.mean
             variance = sum(term.variance for term in [
                 mutual_info, entropy1, entropy2, entropy_joint])
             error = np.sqrt(variance)
-            assert_less_equal(abs(actual - expected), 2.575 * error)
+            assert_less_equal(abs(actual - expected), Z_SCORE * error)
 
 
 def _check_marginal_samples_match_scores(protobuf_server, row, fi):
@@ -95,7 +104,7 @@ def _check_marginal_samples_match_scores(protobuf_server, row, fi):
     cond_row = loom.query.protobuf_to_data_row(row.data)
     cond_row[fi] = None
     to_sample = [i == fi for i in range(len(cond_row))]
-    samples = query_server.sample(to_sample, cond_row, 500)
+    samples = query_server.sample(to_sample, cond_row, SAMPLE_COUNT)
     val = samples[0][fi]
     base_score = query_server.score(cond_row)
     if isinstance(val, bool) or isinstance(val, int):
@@ -106,15 +115,14 @@ def _check_marginal_samples_match_scores(protobuf_server, row, fi):
             row[fi] = sample
             probs_dict[sample] = np.exp(query_server.score(row) - base_score)
         if len(probs_dict) == 1:
-            assert_almost_equal(probs_dict[sample], 1., places=2)
+            assert_almost_equal(probs_dict[sample], 1., places=GOF_EXP)
             return
-        if min(probs_dict.values()) < .01:
-            # TODO tests are inaccurate with highly imbalanced data
+        if min(probs_dict.values()) < MIN_CATEGORICAL_PROB:
             return
         gof = discrete_goodness_of_fit(samples, probs_dict, plot=True)
     elif isinstance(val, float):
-        probs = np.exp(query_server.score(sample) - base_score
-                       for sample in samples)
+        probs = np.exp([query_server.score(sample) - base_score
+                       for sample in samples])
         samples = [sample[fi] for sample in samples]
         gof = density_goodness_of_fit(samples, probs, plot=True)
     assert_greater(gof, MIN_GOODNESS_OF_FIT)
